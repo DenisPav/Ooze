93eb20d4109412bcdbb258cdcf8004dd86c2df47
Denis Pavlović <denis.pav@hotmail.com>
2020-03-07 17:11:39 +0100

    Extracted sorter and filter implementations

--- src/Ooze/Filters/IOozeFilterHandler.cs
+++ src/Ooze/Filters/IOozeFilterHandler.cs
@@ -1,92 +1,9 @@
-﻿using Ooze.Configuration;
-using Superpower;
-using Superpower.Model;
-using Superpower.Parsers;
-using System;
-using System.Linq;
-using static System.Linq.Expressions.Expression;
+﻿using System.Linq;
 
 namespace Ooze.Filters
 {
     public interface IOozeFilterHandler
     {
         IQueryable<TEntity> Handle<TEntity>(IQueryable<TEntity> query, string filters);
-    }
-
-    public class OozeFilterHandler : IOozeFilterHandler
-    {
-        const string Where = nameof(Where);
-        static readonly Type _queryableType = typeof(Queryable);
-
-        readonly OozeConfiguration _config;
-
-        public OozeFilterHandler(
-            OozeConfiguration config)
-        {
-            _config = config;
-        }
-
-        public IQueryable<TEntity> Handle<TEntity>(IQueryable<TEntity> query, string filters)
-        {
-            var entity = typeof(TEntity);
-            var configuration = _config.EntityConfigurations.FirstOrDefault(config => config.Type.Equals(entity));
-
-            var propertyParsers = configuration.Filters.Select(def => Span.EqualToIgnoreCase(def.Name)).ToList();
-            var propertyParser = propertyParsers.Aggregate<TextParser<TextSpan>, TextParser<TextSpan>>(null, (accumulator, singlePropertyParser) =>
-            {
-                if (accumulator == null)
-                    return singlePropertyParser;
-
-                return accumulator.Or(singlePropertyParser);
-            }).OptionalOrDefault(new TextSpan(string.Empty));
-
-            var operationParsers = _config.OperationsMap.Keys.Select(Span.EqualToIgnoreCase).ToList();
-            var operationParser = operationParsers.Aggregate<TextParser<TextSpan>, TextParser<TextSpan>>(null, (accumulator, singlePropertyParser) =>
-            {
-                if (accumulator == null)
-                    return singlePropertyParser;
-
-                return accumulator
-                    .Try()
-                    .Or(singlePropertyParser);
-            });
-
-            var valueParser = Span.WithAll(_ => true).OptionalOrDefault(new TextSpan(string.Empty));
-            var filterParser = (from property in propertyParser
-                                from operation in operationParser
-                                from value in valueParser
-                                select (property, operation, value));
-
-            var splittedFilters = filters.Split(',').ToList();
-            var parsedFilters = splittedFilters.Select(filterParser.Parse).ToList();
-
-            var appliedFilters = parsedFilters.Join(
-                configuration.Filters,
-                x => x.property.ToString(),
-                x => x.Name,
-                (x, y) => (parsed: x, def: y),
-                StringComparer.InvariantCultureIgnoreCase)
-                .ToList();
-
-            foreach (var (parsed, def) in appliedFilters)
-            {
-                var expr = query.Expression;
-                var typings = new[] { entity };
-
-                var value = System.Convert.ChangeType(parsed.value.ToString(), def.Type);
-                var constValueExpr = Constant(value);
-                var operationExpr = _config.OperationsMap[parsed.operation.ToString()](def.Expression, constValueExpr);
-                var lambda = Lambda(operationExpr, configuration.Param);
-
-                var quoteExpr = Quote(lambda);
-
-                var callExpr = Call(_queryableType, Where, typings, expr, quoteExpr);
-
-                query = query.Provider
-                    .CreateQuery<TEntity>(callExpr);
-            }
-
-            return query;
-        }
     }
 }
--- src/Ooze/Sorters/IOozeSorterHandler.cs
+++ src/Ooze/Sorters/IOozeSorterHandler.cs
@@ -1,79 +1,9 @@
-﻿using Ooze.Configuration;
-using System;
-using System.Linq;
-using System.Linq.Expressions;
-using static System.Linq.Expressions.Expression;
+﻿using System.Linq;
 
 namespace Ooze.Sorters
 {
     public interface IOozeSorterHandler
     {
         IQueryable<TEntity> Handle<TEntity>(IQueryable<TEntity> query, string sorters);
-    }
-
-    public class OozeSorterHandler : IOozeSorterHandler
-    {
-        const string OrderBy = nameof(OrderBy);
-        const string ThenBy = nameof(ThenBy);
-        const string ThenByDescending = nameof(ThenByDescending);
-        const string OrderByDescending = nameof(OrderByDescending);
-        const char NegativeOrderChar = '-';
-
-        static readonly Type _queryableType = typeof(Queryable);
-
-        readonly OozeConfiguration _config;
-
-        public OozeSorterHandler(
-            OozeConfiguration config)
-        {
-            _config = config;
-        }
-
-        public IQueryable<TEntity> Handle<TEntity>(IQueryable<TEntity> query, string sorters)
-        {
-            var entity = typeof(TEntity);
-            var configuration = _config.EntityConfigurations.FirstOrDefault(config => config.Type.Equals(entity));
-
-            var modelSorters = sorters.Split(',').Select(sorter => sorter.Trim())
-                .Select(sorter => new
-                {
-                    Ascending = !sorter.StartsWith(NegativeOrderChar) ? true : false,
-                    Sorter = sorter.StartsWith(NegativeOrderChar) ? new string(sorter.Skip(1).ToArray()) : sorter
-                })
-                .ToList();
-
-            var appliedSorters = modelSorters.Join(
-                configuration.Sorters,
-                x => x.Sorter,
-                x => x.Name,
-                (x, y) => (y.Name, y.Expression, y.Type, x.Ascending),
-                StringComparer.InvariantCultureIgnoreCase)
-                .ToList();
-
-            for (int i = 0; i < appliedSorters.Count(); i++)
-            {
-                var expr = query.Expression;
-                var sorter = appliedSorters[i];
-                MethodCallExpression callExpr;
-                var quoteExpr = Quote(sorter.Item2);
-                var typings = new Type[] { entity, sorter.Item3 };
-
-                if (i == 0)
-                {
-                    var method = sorter.Ascending ? OrderBy : OrderByDescending;
-                    callExpr = Call(_queryableType, method, typings, expr, quoteExpr);
-                }
-                else
-                {
-                    var method = sorter.Ascending ? ThenBy : ThenByDescending;
-                    callExpr = Call(_queryableType, method, typings, expr, quoteExpr);
-                }
-
-                query = query.Provider
-                    .CreateQuery<TEntity>(callExpr);
-            }
-
-            return query;
-        }
     }
 }
