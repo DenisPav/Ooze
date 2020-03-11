fc3b5cf24f36622708dfe65b81e93c567f01bffa
Denis Pavlović <denis.pav@hotmail.com>
2020-03-11 21:12:39 +0100

    Extracted groups of code in FilterHandler

--- src/Ooze/Filters/OozeFilterHandler.cs
+++ src/Ooze/Filters/OozeFilterHandler.cs
@@ -3,7 +3,9 @@ using Superpower;
 using Superpower.Model;
 using Superpower.Parsers;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using static System.Linq.Expressions.Expression;
 
 namespace Ooze.Filters
@@ -25,8 +27,65 @@ namespace Ooze.Filters
         {
             var entity = typeof(TEntity);
             var configuration = _config.EntityConfigurations[entity];
-            var whiteSpaceParser = Character.WhiteSpace.Many();
 
+            var filterParser = CreateParser(configuration);
+            var appliedFilters = GetAppliedFilters(filters, configuration, filterParser);
+
+            foreach (var (parsed, def) in appliedFilters)
+            {
+                var expr = query.Expression;
+                var callExpr = CreateFilterExpression(entity, configuration, parsed, def, expr);
+
+                query = query.Provider
+                    .CreateQuery<TEntity>(callExpr);
+            }
+
+            return query;
+        }
+
+        MethodCallExpression CreateFilterExpression(
+            Type entity,
+            OozeEntityConfiguration configuration,
+            (TextSpan property, TextSpan operation, TextSpan value) parsed,
+            ParsedExpressionDefinition def,
+            Expression expr)
+        {
+            var typings = new[] { entity };
+            var value = System.Convert.ChangeType(parsed.value.ToString(), def.Type);
+
+            var constValueExpr = Constant(value);
+            var operationExpr = _config.OperationsMap[parsed.operation.ToString()](def.Expression, constValueExpr);
+            var lambda = Lambda(operationExpr, configuration.Param);
+            var quoteExpr = Quote(lambda);
+
+            var callExpr = Call(_queryableType, Where, typings, expr, quoteExpr);
+            return callExpr;
+        }
+
+        IEnumerable<((TextSpan property, TextSpan operation, TextSpan value) parsed, ParsedExpressionDefinition def)> GetAppliedFilters(
+            string filters,
+            OozeEntityConfiguration configuration,
+            TextParser<(TextSpan property, TextSpan operation, TextSpan value)> filterParser)
+        {
+            var splittedFilters = filters.Split(',');
+            var parsedFilters = splittedFilters.Select(filterParser.TryParse)
+                .Where(result => result.HasValue)
+                .Select(result => result.Value);
+
+            var appliedFilters = parsedFilters.Join(
+                configuration.Filters,
+                x => x.property.ToString(),
+                x => x.Name,
+                (x, y) => (parsed: x, def: y),
+                StringComparer.InvariantCultureIgnoreCase)
+                .ToList();
+
+            return appliedFilters;
+        }
+
+        TextParser<(TextSpan property, TextSpan operation, TextSpan value)> CreateParser(OozeEntityConfiguration configuration)
+        {
+            var whiteSpaceParser = Character.WhiteSpace.Many();
             var propertyParsers = configuration.Filters.Select(def => Span.EqualToIgnoreCase(def.Name).Between(whiteSpaceParser, whiteSpaceParser)).ToList();
             var propertyParser = propertyParsers.Aggregate<TextParser<TextSpan>, TextParser<TextSpan>>(null, (accumulator, singlePropertyParser) =>
             {
@@ -53,39 +112,7 @@ namespace Ooze.Filters
                                 from value in valueParser
                                 select (property, operation, value));
 
-            var splittedFilters = filters.Split(',').ToList();
-            var parsedFilters = splittedFilters.Select(filterParser.TryParse)
-                .Where(result => result.HasValue)
-                .Select(result => result.Value)
-                .ToList();
-
-            var appliedFilters = parsedFilters.Join(
-                configuration.Filters,
-                x => x.property.ToString(),
-                x => x.Name,
-                (x, y) => (parsed: x, def: y),
-                StringComparer.InvariantCultureIgnoreCase)
-                .ToList();
-
-            foreach (var (parsed, def) in appliedFilters)
-            {
-                var expr = query.Expression;
-                var typings = new[] { entity };
-
-                var value = System.Convert.ChangeType(parsed.value.ToString(), def.Type);
-                var constValueExpr = Constant(value);
-                var operationExpr = _config.OperationsMap[parsed.operation.ToString()](def.Expression, constValueExpr);
-                var lambda = Lambda(operationExpr, configuration.Param);
-
-                var quoteExpr = Quote(lambda);
-
-                var callExpr = Call(_queryableType, Where, typings, expr, quoteExpr);
-
-                query = query.Provider
-                    .CreateQuery<TEntity>(callExpr);
-            }
-
-            return query;
+            return filterParser;
         }
     }
 }
