8842fa5177c2071d812b57b3a7a7a89ca589935e
Denis Pavlović <denis.pav@hotmail.com>
2020-03-15 22:38:04 +0100

    Added test for sorters

--- tests/Ooze.Tests/OozeCustomProviderProviderTests.cs
+++ tests/Ooze.Tests/OozeCustomProviderProviderTests.cs
@@ -1,5 +1,6 @@
 ﻿using NSubstitute;
 using Ooze.Filters;
+using Ooze.Sorters;
 using System.Linq;
 using Xunit;
 
@@ -18,6 +19,17 @@ namespace Ooze.Tests
             Assert.True(context.OozeCustomProviderProvider.FiltersFor<NotUsedEntity>().Count() == 0);
         }
 
+        [Fact]
+        public void Should_Correctly_Resolve_Sorters()
+        {
+            var context = new OozeCustomProviderProviderContext();
+
+            Assert.True(context.OozeCustomProviderProvider.SortersFor<object>().Count() == 1);
+            Assert.True(context.OozeCustomProviderProvider.SortersFor<SampleEntity>().Count() == 1);
+
+            Assert.True(context.OozeCustomProviderProvider.SortersFor<NotUsedEntity>().Count() == 0);
+        }
+
         public class SampleEntity
         { }
 
@@ -29,11 +41,13 @@ namespace Ooze.Tests
             public OozeCustomProviderProvider OozeCustomProviderProvider { get; set; }
             public IOozeFilterProvider<object> FilterProvider1 { get; } = Substitute.For<IOozeFilterProvider<object>>();
             public IOozeFilterProvider<SampleEntity> FilterProvider2 { get; } = Substitute.For<IOozeFilterProvider<SampleEntity>>();
+            public IOozeSorterProvider<object> SorterProvider1 { get; } = Substitute.For<IOozeSorterProvider<object>>();
+            public IOozeSorterProvider<SampleEntity> SorterProvider2 { get; } = Substitute.For<IOozeSorterProvider<SampleEntity>>();
 
 
             public OozeCustomProviderProviderContext()
             {
-                OozeCustomProviderProvider = new OozeCustomProviderProvider(new IOozeProvider[] { FilterProvider1, FilterProvider2 });
+                OozeCustomProviderProvider = new OozeCustomProviderProvider(new IOozeProvider[] { FilterProvider1, FilterProvider2, SorterProvider1, SorterProvider2 });
             }
         }
     }
