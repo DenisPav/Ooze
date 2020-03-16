c6c6c742dfc867ce53f95d3336c25047db7b2a61
Denis Pavlović <denis.pav@hotmail.com>
2020-03-16 20:16:22 +0100

    Initial FilterHandler refactoring

--- src/Ooze/Filters/OozeFilterHandler.cs
+++ src/Ooze/Filters/OozeFilterHandler.cs
@@ -2,21 +2,13 @@
 using Superpower;
 using Superpower.Model;
 using Superpower.Parsers;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Linq.Expressions;
-using static System.Linq.Expressions.Expression;
 
 namespace Ooze.Filters
 {
     internal class OozeFilterHandler : IOozeFilterHandler
     {
-        const string Where = nameof(Where);
-        static readonly Type _queryableType = typeof(Queryable);
-        static readonly Type _stringType = typeof(string);
-
         readonly IOozeCustomProviderProvider _customProviderProvider;
         readonly OozeConfiguration _config;
 
@@ -31,36 +23,16 @@ namespace Ooze.Filters
         public IQueryable<TEntity> Handle<TEntity>(IQueryable<TEntity> query, string filters)
             where TEntity : class
         {
-            var entity = typeof(TEntity);
-            var configuration = _config.EntityConfigurations[entity];
+            var configuration = _config.EntityConfigurations[typeof(TEntity)];
             var customProviders = _customProviderProvider.FiltersFor<TEntity>();
 
             var filterParser = CreateParser(configuration, customProviders);
             var parsedFilters = GetParsedFilters(filters, filterParser);
-            var appliedConfigurationFilters = GetAppliedFilters(parsedFilters, configuration);
 
-            foreach (var filter in parsedFilters)
-            {
-                var expr = query.Expression;
+            IQueryable<TEntity> Accumulator(IQueryable<TEntity> accumulator, FilterParserResult filter) 
+                => OozeQueryableCreator.ForFilter(accumulator, configuration, filter, customProviders, _config.OperationsMap);
 
-                var configFilter = appliedConfigurationFilters.SingleOrDefault(configFilter => string.Equals(configFilter.Name, filter.Property, StringComparison.InvariantCultureIgnoreCase));
-                if (configFilter != null)
-                {
-                    var callExpr = CreateFilterExpression(entity, configuration, filter, configFilter, expr);
-                    query = query.Provider
-                        .CreateQuery<TEntity>(callExpr);
-                }
-                else
-                {
-                    var providerQuery = customProviders.SingleOrDefault(provider => string.Equals(provider.Name, filter.Property, StringComparison.InvariantCultureIgnoreCase))
-                        ?.ApplyFilter(query, filter);
-
-                    if (providerQuery != null)
-                        query = providerQuery;
-                }
-            }
-
-            return query;
+            return parsedFilters.Aggregate(query, Accumulator);
         }
 
         IEnumerable<FilterParserResult> GetParsedFilters(
@@ -73,47 +45,6 @@ namespace Ooze.Filters
                 .Select(result => result.Value);
 
             return parsedFilters.ToList();
-        }
-
-        MethodCallExpression CreateFilterExpression(
-            Type entity,
-            OozeEntityConfiguration configuration,
-            FilterParserResult parsedFilter,
-            ParsedExpressionDefinition def,
-            Expression expr)
-        {
-            var typings = new[] { entity };
-
-            var typeConverter = TypeDescriptor.GetConverter(def.Type);
-            var value = typeConverter.CanConvertFrom(_stringType)
-                ? typeConverter.ConvertFrom(parsedFilter.Value)
-                : System.Convert.ChangeType(parsedFilter.Value, def.Type);
-
-            var constValueExpr = Constant(value);
-            var operationExpr = _config.OperationsMap[parsedFilter.Operation](def.Expression, constValueExpr);
-            var lambda = Lambda(operationExpr, configuration.Param);
-            var quoteExpr = Quote(lambda);
-
-            var callExpr = Call(_queryableType, Where, typings, expr, quoteExpr);
-            return callExpr;
-        }
-
-        IEnumerable<ParsedExpressionDefinition> GetAppliedFilters(
-            IEnumerable<FilterParserResult> parsedFilters,
-            OozeEntityConfiguration configuration)
-        {
-            var distinctParsedFilters = parsedFilters.Select(filter => filter.Property)
-                .Distinct();
-
-            var appliedFilters = distinctParsedFilters.Join(
-                configuration.Filters,
-                parsedFilter => parsedFilter,
-                configuredFilter => configuredFilter.Name,
-                (parsedFilter, configuredFilter) => configuredFilter,
-                StringComparer.InvariantCultureIgnoreCase)
-                .ToList();
-
-            return appliedFilters;
         }
 
         TextParser<FilterParserResult> CreateParser(OozeEntityConfiguration configuration, IEnumerable<IOozeProvider> customProviders)
---
+++ src/Ooze/Filters/OozeQueryableCreator.cs
@@ -0,0 +1,42 @@
+﻿using Ooze.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Ooze.Expressions.OozeExpressionCreator;
+
+namespace Ooze.Filters
+{
+    internal static class OozeQueryableCreator
+    {
+        public static IQueryable<TEntity> ForFilter<TEntity>(
+            IQueryable<TEntity> query,
+            OozeEntityConfiguration entityConfiguration,
+            FilterParserResult parsedFilter,
+            IEnumerable<IOozeFilterProvider<TEntity>> customFilterProviders,
+            IReadOnlyDictionary<string, Operation> operationsMap)
+            where TEntity : class
+        {
+            var entityType = typeof(TEntity);
+            //this might throw??? check and make sure
+            var filters = entityConfiguration.Filters;
+            var filter = filters.SingleOrDefault(configFilter => string.Equals(configFilter.Name, parsedFilter.Property, StringComparison.InvariantCultureIgnoreCase));
+
+            if (filter != null)
+            {
+                var callExpr = FilterExpression<TEntity>(entityConfiguration, parsedFilter, filter, query.Expression, operationsMap[parsedFilter.Operation]);
+                return query.Provider
+                    .CreateQuery<TEntity>(callExpr);
+            }
+            else
+            {
+                var providerQuery = customFilterProviders.SingleOrDefault(provider => string.Equals(provider.Name, parsedFilter.Property, StringComparison.InvariantCultureIgnoreCase))
+                       ?.ApplyFilter(query, parsedFilter);
+
+                if (providerQuery != null)
+                    return providerQuery;
+            }
+
+            return query;
+        }
+    }
+}
