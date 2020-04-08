93ba1fb8a9d401702a61e01cabd203b3f07fe292
DenisPav <denis.pav@hotmail.com>
2020-04-08 10:03:34 +0200

    Extracted SelectionHandler to specific file

--- src/Ooze/Selections/IOozeSelectionHandler.cs
+++ src/Ooze/Selections/IOozeSelectionHandler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using static System.Linq.Expressions.Expression;
+﻿using System.Linq;
 
 namespace Ooze.Selections
 {
@@ -8,35 +6,5 @@ namespace Ooze.Selections
     {
         IQueryable<TEntity> Handle<TEntity>(IQueryable<TEntity> query, string fields)
             where TEntity : class;
-    }
-
-    internal class OozeSelectionHandler : IOozeSelectionHandler
-    {
-        public IQueryable<TEntity> Handle<TEntity>(
-            IQueryable<TEntity> query,
-            string fields)
-            where TEntity : class
-        {
-            var paramExpr = Parameter(
-                   typeof(TEntity),
-                   typeof(TEntity).Name
-               );
-
-            var bindExpressions = typeof(TEntity).GetProperties()
-                .Where(prop => fields.Contains(prop.Name, StringComparison.InvariantCultureIgnoreCase))
-                .Select(prop => Bind(prop, MakeMemberAccess(paramExpr, prop)));
-
-            var lambda = Lambda<Func<TEntity, TEntity>>(
-                MemberInit(
-                    New(
-                        typeof(TEntity).GetConstructors().First()
-                    ),
-                    bindExpressions
-                ),
-                paramExpr
-            );
-
-            return query.Select(lambda);
-        }
     }
 }
