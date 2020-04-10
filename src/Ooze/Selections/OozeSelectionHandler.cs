dd470dadd0b1a5413bfc72a8b3419e897ce3917a
Denis Pavlović <denis.pav@hotmail.com>
2020-04-10 21:51:26 +0200

    Added correct support for nested collections

--- src/Ooze/Selections/OozeSelectionHandler.cs
+++ src/Ooze/Selections/OozeSelectionHandler.cs
@@ -3,12 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using static System.Linq.Expressions.Expression;
 
 namespace Ooze.Selections
 {
     internal class OozeSelectionHandler : IOozeSelectionHandler
     {
+        class FieldDefinition
+        {
+            public string Property { get; set; }
+            public IList<FieldDefinition> Children { get; set; } = new List<FieldDefinition>();
+        }
+
         public IQueryable<TEntity> Handle<TEntity>(
             IQueryable<TEntity> query,
             string fields)
@@ -19,22 +26,138 @@ namespace Ooze.Selections
                    typeof(TEntity).Name
             );
 
-            var splitted = fields.Split(',');
-            var simpleFields = splitted.Where(field => !field.Contains(".")).ToList();
-            var complexFields = splitted.Except(simpleFields).ToList();
+            //name
+            //id
+            //comments
+            //comments.id
+            //comments.date
+            //comments.users.id
+            //comments.users.name
 
-            var bindExpressionsSimpleFields = typeof(TEntity).GetProperties()
-                .Where(prop => simpleFields.Contains(prop.Name, StringComparer.InvariantCultureIgnoreCase))
-                .Select(prop => Bind(prop, MakeMemberAccess(paramExpr, prop)));
+            var splitted = fields.Split(',')
+                .OrderByDescending(def => def.Count(@char => @char == '.'))
+                .ToList();
 
-            IEnumerable<MemberAssignment> bindExpressionsComplexFields = CreateComplexMemberAssignments<TEntity>(paramExpr, complexFields);
+            IList<FieldDefinition> Parse(IEnumerable<string> fieldDefinitions, IList<FieldDefinition> existingDefinitions = null)
+            {
+                existingDefinitions ??= new List<FieldDefinition>();
+
+                foreach (var fieldDefinition in fieldDefinitions)
+                {
+                    if (!fieldDefinition.Contains('.'))
+                    {
+                        if (!existingDefinitions.Any(def => def.Property.Equals(fieldDefinition, StringComparison.InvariantCultureIgnoreCase)))
+                        {
+                            existingDefinitions.Add(new FieldDefinition
+                            {
+                                Property = fieldDefinition
+                            });
+                        }
+                    }
+                    else
+                    {
+                        var fieldDefinitionParts = fieldDefinition.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries);
+                        var containerDef = existingDefinitions.FirstOrDefault(def => def.Property.Equals(fieldDefinitionParts[0], StringComparison.InvariantCultureIgnoreCase));
+
+                        if (containerDef is null)
+                        {
+                            var newDef = new FieldDefinition
+                            {
+                                Property = fieldDefinitionParts[0]
+                            };
+                            newDef.Children = Parse(new[] { string.Join('.', fieldDefinitionParts.Skip(1)) }, newDef.Children).ToList();
+
+                            existingDefinitions.Add(newDef);
+                        }
+                        else
+                        {
+                            containerDef.Children = Parse(new[] { string.Join('.', fieldDefinitionParts.Skip(1)) }, containerDef.Children).ToList();
+                        }
+                    }
+                }
+
+                return existingDefinitions;
+            }
+
+            var results = Parse(splitted);
+
+            IEnumerable<MemberAssignment> CreateAssignments(ParameterExpression rootExpression, Type type, IEnumerable<FieldDefinition> fieldDefinitions)
+            {
+                var assignemnts = new List<MemberAssignment>();
+                var typeProperties = type.GetProperties();
+
+                foreach (var fieldDefinition in fieldDefinitions)
+                {
+                    if (!fieldDefinition.Children.Any())
+                    {
+                        var targetProp = typeProperties.FirstOrDefault(prop => prop.Name.Equals(fieldDefinition.Property, StringComparison.InvariantCultureIgnoreCase));
+
+                        if (targetProp is { })
+                        {
+                            var bind = Bind(targetProp, MakeMemberAccess(rootExpression, targetProp));
+                            assignemnts.Add(bind);
+                        }
+                    }
+                    else
+                    {
+                        var targetProp = typeProperties.FirstOrDefault(prop => prop.Name.Equals(fieldDefinition.Property, StringComparison.InvariantCultureIgnoreCase));
+                        if (targetProp is { } && typeof(IEnumerable).IsAssignableFrom(targetProp.PropertyType))
+                        {
+                            var propertyType = targetProp.PropertyType.GetGenericArguments().First();
+                            var newRootParamExpr = Parameter(propertyType, targetProp.Name);
+
+                            var nestedAssignments = CreateAssignments(newRootParamExpr, propertyType, fieldDefinition.Children);
+
+                            var lambda = Lambda(
+                                MemberInit(
+                                    New(
+                                        propertyType.GetConstructors().First()
+                                        ),
+                                    nestedAssignments
+                                    ),
+                                newRootParamExpr
+                                );
+
+                            var selectCallExpr = Call(
+                                typeof(Enumerable),
+                                "Select",
+                                new[] {
+                                    propertyType,
+                                    propertyType
+                                },
+                                MakeMemberAccess(rootExpression, targetProp),
+                                lambda
+                            );
+
+                            var toListCallExpr = Call(
+                                typeof(Enumerable),
+                                "ToList",
+                                new[] {
+                                    propertyType
+                                },
+                                selectCallExpr
+                            );
+
+                            assignemnts.Add(Bind(
+                                targetProp,
+                                toListCallExpr
+                                ));
+                        }
+                    }
+                }
+
+                return assignemnts;
+            }
+
+            var assignments = CreateAssignments(paramExpr, typeof(TEntity), results);
+
 
             var lambda = Lambda<Func<TEntity, TEntity>>(
                 MemberInit(
                     New(
                         typeof(TEntity).GetConstructors().First()
                     ),
-                    bindExpressionsSimpleFields.Concat(bindExpressionsComplexFields)
+                    assignments
                 ),
                 paramExpr
             );
@@ -80,7 +203,7 @@ namespace Ooze.Selections
                     var selectCallExpr = Call(
                         typeof(Enumerable),
                         "Select",
-                        new [] {
+                        new[] {
                             comment,
                             comment
                         },
