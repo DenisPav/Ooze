654d2219853c1a9a6ac9af975850abf2cdd406b5
Denis Pavlović <denis.pav@hotmail.com>
2023-05-12 22:50:49 +0200

    Add needed linked files, Add wrapped parameter value for generated expression

--- tests/Ooze.Typed.Web/OozeConfiguration/BlogFiltersProvider.cs
+++ tests/Ooze.Typed.Web/OozeConfiguration/BlogFiltersProvider.cs
@@ -6,7 +6,6 @@ public class BlogFiltersProvider : IOozeFilterProvider<Blog, BlogFilters>
     public IEnumerable<IFilterDefinition<Blog, BlogFilters>> GetFilters()
     {
         return Filters.CreateFor<Blog, BlogFilters>()
-            // .StartsWith(blog => blog.Name, filter => filter.Name)
             .Equal(blog => blog.Id, filter => filter.BlogId)
             .Range(blog => blog.Id, filter => filter.BlogRange)
             .In(blog => blog.Id, filter => filter.BlogIds)
