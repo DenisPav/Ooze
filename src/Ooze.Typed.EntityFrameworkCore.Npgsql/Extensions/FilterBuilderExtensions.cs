b32cceb7ee40a5cd49436e884ac58099e2e29837
Denis Pavlović <denis.pav@hotmail.com>
2023-05-28 13:46:24 +0200

    Add Postgres project

---
+++ src/Ooze.Typed.EntityFrameworkCore.Npgsql/Extensions/FilterBuilderExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Ooze.Typed.Expressions;
+using Ooze.Typed.Filters;
+using static System.Linq.Expressions.Expression;
+
+namespace Ooze.Typed.EntityFrameworkCore.Npgsql.Extensions;
+
+/// <summary>
+/// Postgres extensions for FilterBuilder
+/// </summary>
+public static class FilterBuilderExtensions
+{
+    private static readonly Type DbFunctionsExtensionsType = typeof(NpgsqlDbFunctionsExtensions);
+    // ReSharper disable once InconsistentNaming
+    private const string ILikeMethod = nameof(NpgsqlDbFunctionsExtensions.ILike);
+    private static readonly MemberExpression EfPropertyExpression = Property(null, typeof(EF), nameof(EF.Functions));
+    
+    /// <summary>
+    /// Creates a ILike filter over specified property and filter
+    /// </summary>
+    /// <param name="filterBuilder">Instance of <see cref="IFilterBuilder{TEntity,TFilter}"/></param>
+    /// <param name="dataExpression">Expression targeting entity property</param>
+    /// <param name="filterFunc">Filtering delegate targeting property with details if filter should apply</param>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    /// <typeparam name="TFilter">Filter type</typeparam>
+    /// <typeparam name="TProperty">Targeted property type</typeparam>
+    /// <returns>Instance of builder for fluent building of multiple filter definitions</returns>
+    public static IFilterBuilder<TEntity, TFilter> InsensitiveLike<TEntity, TFilter, TProperty>(
+        this IFilterBuilder<TEntity, TFilter> filterBuilder,
+        Expression<Func<TEntity, TProperty?>> dataExpression,
+        Func<TFilter, string?> filterFunc)
+    {
+        bool FilterShouldRun(TFilter filter) => filterFunc(filter) != null;
+
+        Expression<Func<TEntity, bool>> FilterExpressionFactory(TFilter filter)
+        {
+            var filterValue = filterFunc(filter);
+            var memberAccessExpression = BasicExpressions.GetMemberExpression(dataExpression.Body);
+            var parameterExpression = BasicExpressions.ExtractParameterExpression(memberAccessExpression);
+            var constantExpression = BasicExpressions.GetWrappedConstantExpression(filterValue);
+            var callExpression = Call(
+                DbFunctionsExtensionsType,
+                ILikeMethod,
+                Type.EmptyTypes,
+                EfPropertyExpression,
+                memberAccessExpression!,
+                constantExpression);
+
+            return Lambda<Func<TEntity, bool>>(callExpression, parameterExpression);
+        }
+
+        filterBuilder.Add(FilterShouldRun, FilterExpressionFactory);
+        return filterBuilder;
+    }
+}
