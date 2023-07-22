8073d71898d17a81844e89f4202f9a0b2426e94a
Denis Pavlović <denis.pav@hotmail.com>
2023-07-22 13:09:19 +0200

    Add MariaDb migrations

--- tests/Ooze.Typed.Web/Program.cs
+++ tests/Ooze.Typed.Web/Program.cs
@@ -2,21 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Ooze.Typed.Extensions;
 using Ooze.Typed.Web;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<DatabaseContext>(opts =>
     opts.UseSqlite("Data Source=./database.db;").EnableSensitiveDataLogging());
 builder.Services.AddDbContext<SqlServerDatabaseContext>(opts =>
     opts.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")).EnableSensitiveDataLogging());
 builder.Services.AddDbContext<PostgresDatabaseContext>(opts =>
     opts.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")).EnableSensitiveDataLogging());
+var serverVersion = ServerVersion.Create(new Version("10.7.4"), ServerType.MariaDb);
 builder.Services.AddDbContext<MariaDbDatabaseContext>(opts =>
-        opts.UseMySql(ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MariaDb")), options =>
-            options.EnableRetryOnFailure(
-                maxRetryCount: 5,
-                maxRetryDelay: TimeSpan.FromSeconds(30),
-                errorNumbersToAdd: null)
-        ).EnableSensitiveDataLogging());
+    opts.UseMySql(builder.Configuration.GetConnectionString("MariaDb"), serverVersion).EnableSensitiveDataLogging());
 builder.Services.AddHostedService<SeedService>();
 builder.Services.AddOozeTyped()
     .Add<BlogFiltersProvider>()
