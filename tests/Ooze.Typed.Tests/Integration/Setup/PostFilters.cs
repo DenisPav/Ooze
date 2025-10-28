5566d0beea1d15113e1a59902f1d895976ee6f9d
Denis Pavlović <denis.pav@hotmail.com>
2025-10-28 22:25:35 +0100

    Add Glob filters

--- tests/Ooze.Typed.Tests/Integration/Setup/PostFilters.cs
+++ tests/Ooze.Typed.Tests/Integration/Setup/PostFilters.cs
@@ -3,5 +3,6 @@
 namespace Ooze.Typed.Tests.Integration.Setup;
 
 public record PostFilters(long? Id, string? Name, bool? Enabled, DateTime? Date);
+public record PostGlobFilters(string GlobId);
 public record PostInFilters(IEnumerable<long> Ids, IEnumerable<string> Names, IEnumerable<DateTime> Dates);
 public record PostRangeFilters(RangeFilter<long> Ids, RangeFilter<string> Names, RangeFilter<DateTime> Dates);
